e705c7f68a9eb0ca3770842ddc3d055fdfb84065
MissYoruwara <cecel31@free.fr>
2017-04-27 16:14:54 +0200

    Ajout de la classe Season

---
+++ Catcheur-Manager/Catcheur-Manager/Models/Season.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catcheur_Manager.Models
+{
+    class Season
+    {
+        public int Profit { get; set; }
+        public Match CurrentMatch { get; set; }
+        public int Rate { get; set; }
+
+        public Season(int profit, Match currentMatch, int rate)
+        {
+            Profit = profit;
+            CurrentMatch = currentMatch;
+            Rate = rate;
+        }
+    }
+
+}
