1ffdacbe95d093f644fc44e74d3cc23423ab620a
MissYoruwara <cecel31@free.fr>
2017-04-27 15:07:46 +0200

    ajout classe Match

---
+++ Catcheur-Manager/Catcheur-Manager/Models/Match.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catcheur_Manager.Models
+{
+    class Match
+    {
+        protected int Iteration {get; set; }
+        protected Wrestler FirstWrestler { get; set; }
+        protected Wrestler SecondWrestler { get; set; }
+        public Wrestler Winner { get; set; }
+        public Wrestler Loser { get; set; }
+        public  bool  WayOfWinning { get; set; }
+        public int Profit { get; set; }
+
+        public Match(int iteration, Wrestler firstWrestler, Wrestler secondWrestler, Wrestler winner, Wrestler loser, bool wayOfWinning, int profit)
+        {
+            Iteration = iteration;
+            FirstWrestler = firstWrestler;
+            SecondWrestler = secondWrestler;
+            Winner = winner;
+            Loser = loser;
+            WayOfWinning = wayOfWinning;
+            Profit = profit; 
+        }
+
+        public void CreateMatch()
+        {
+
+        }
+
+    }
+}
